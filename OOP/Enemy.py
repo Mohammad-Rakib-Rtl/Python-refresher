b2aca9118e4342dc9a64c2e93f0dab248121e5c3
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2025-07-26 07:18:00 +0600

    constructor add

--- OOP/Enemy.py
+++ OOP/Enemy.py
@@ -3,3 +3,30 @@ class Enemy:
     type_of_enemy: str
     health_points: int = 10
     attack_damage: int = 1
+
+    ### example of abstraction (which hides the functionality like torch switch)
+    def talk(self):
+        # print(f'I am a {self.type_of_enemy}. Be prepared to fight!')
+        return f'I am a {self.type_of_enemy}. Be prepared to fight!'
+
+    def walk_forward(self):
+        # print(f'{self.type_of_enemy} moves closer to you.')
+        return f'{self.type_of_enemy} moves closer to you.'
+
+    def attack(self):
+        return f'{self.type_of_enemy} attacks for {self.attack_damage} damage.'
+        # print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage.')
+
+
+ogre = Enemy()
+# enemy2 = Enemy()
+# enemy2.health_points = 200
+ogre.type_of_enemy = "Ogre"
+
+# print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage}')
+
+# print(enemy.health_points)
+
+print(ogre.talk())
+print(ogre.walk_forward())
+print(ogre.attack())
--- OOP/Main.py
+++ OOP/Main.py
@@ -1,8 +1,14 @@
 from Enemy import *
 
-enemy = Enemy()
+ogre = Enemy()
 # enemy2 = Enemy()
 # enemy2.health_points = 200
-enemy.type_of_enemy = "Ogre"
+ogre.type_of_enemy = "Ogre"
  
-print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage}')
+# print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage}')
+
+# print(enemy.health_points)
+
+print(ogre.talk())
+print(ogre.walk_forward())
+print(ogre.attack())
