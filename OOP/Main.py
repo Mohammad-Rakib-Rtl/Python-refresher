9cc455adcf5254ff4fadb9e4074d2099abaf5b11
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2025-08-01 22:20:29 +0600

    push full code python crash course

--- OOP/Main.py
+++ OOP/Main.py
@@ -1,6 +1,8 @@
 from Enemy import *
 from Zombie import *
 from Ogre import *
+from Hero import *
+from Weapon import *
 
 # zombie = Zombie("Zombie", 10)
 # ogre = Ogre(30)
@@ -53,39 +55,71 @@ from Ogre import *
 
 #### Polymorphism ####
 
-def battle(e1: Enemy, e2: Enemy):
-    e1.talk()
-    e2.talk()
+# def battle(e1: Enemy, e2: Enemy):
+#     e1.talk()
+#     e2.talk()
+#
+#     while e1.health_points > 0 and e2.health_points > 0:
+#         print('------------------')
+#         e1.special_attack()
+#         e2.special_attack()
+#         print(f'{e1.get_type_of_enemy()}: {e1.health_points} HP left.')
+#         print(f'{e2.get_type_of_enemy()}: {e2.health_points} HP left.')
+#         e2.attack()
+#         e1.health_points -= e2.attack_damage
+#         e1.attack()
+#         e2.health_points -= e1.attack_damage
+#
+#     print('--------------')
+#
+#     if e1.health_points > 0:
+#         print(f'{e1.get_type_of_enemy()} wins!')
+#     else:
+#         print(f'{e2.get_type_of_enemy()} wins!')
 
-    while e1.health_points > 0 and e2.health_points > 0:
+## Composition
+
+def hero_battle(hero: Hero, enemy: Enemy):
+
+
+    while hero.health_points > 0 and enemy.health_points > 0:
         print('------------------')
-        e1.special_attack()
-        e2.special_attack()
-        print(f'{e1.get_type_of_enemy()}: {e1.health_points} HP left.')
-        print(f'{e2.get_type_of_enemy()}: {e2.health_points} HP left.')
-        e2.attack()
-        e1.health_points -= e2.attack_damage
-        e1.attack()
-        e2.health_points -= e1.attack_damage
+
+        enemy.special_attack()
+
+        print(f'Hero: {hero.health_points} HP left.')
+        print(f'{enemy.get_type_of_enemy()}: {enemy.health_points} HP left.')
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
 
     print('--------------')
 
-    if e1.health_points > 0:
-        print(f'{e1.get_type_of_enemy()} wins!')
+    if hero.health_points > 0:
+        print(f'Hero wins!')
     else:
-        print(f'{e2.get_type_of_enemy()} wins!')
+        print(f'{enemy.get_type_of_enemy()} wins!')
+
+
+
 
 
 zombie = Zombie(10, 1)
 ogre = Ogre(20, 3)
-range_creep = Enemy("Ranger", 10, 2)
+ogrerange_creep = Enemy("Ranger", 10, 2)
 
-battle(zombie, ogre)
-print(range_creep.special_attack())
-print(ogre.special_attack())
+hero = Hero(10, 1)
+weapon = Weapon('Sword', 5)
+hero.weapon = weapon
+hero.equip_weapon()
+
+# battle(zombie, ogre)
+# print(range_creep.special_attack())
+# print(ogre.special_attack())
 
 
-
+hero_battle(hero, zombie)
 
 
 
